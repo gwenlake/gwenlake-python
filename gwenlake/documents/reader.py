735ee068a13da2494135416652e217d51f55f74b
Sylvain Barthélémy <sylbarth@gmail.com>
2023-09-08 16:40:39 +0200

    reader

--- gwenlake/documents/reader.py
+++ gwenlake/documents/reader.py
@@ -5,8 +5,11 @@ from gwenlake.api import APIClient
 class DocumentTextReader:
 
     def __init__(self, file):
-        files = {'file': (file, open(file,'rb'))}
-        self.content = APIClient().fetch(f"/documents/readtext", files=files, method="post")
+        try:
+            files = {'file': (file, open(file,'rb'))}
+            self.content = APIClient().fetch(f"/documents/readtext", files=files, method="post")
+        except Exception as e:
+            print(e)
     
     def get_content(self):
         return self.content
