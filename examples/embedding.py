812f1c086341af8732be6974ff6453fa4305f491
Sylvain Barthélémy <sylbarth@gmail.com>
2023-09-13 16:27:12 +0200

    sqlalchemy repo

--- examples/embedding.py
+++ examples/embedding.py
@@ -7,9 +7,13 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 dotenv.load_dotenv()
 
 documents = [
-    "My name is Sylvain",
-    "My name is not Sylvain",
+    "Do Not Watch This Movie!",
+    "This movie is SOOOO funny!!! The acting is WONDERFUL.",
+    "Everything about this movie is horrible, from the acting to the editing.",
 ]
 
-embeddings = gwenlake.api.APIClient().embed_documents(documents)
+embeddings = gwenlake.api.Client().embed_documents(documents)
+
 print(pd.DataFrame(embeddings))
+
+
