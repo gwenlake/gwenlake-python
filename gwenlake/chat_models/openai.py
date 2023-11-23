0d6a307b48dec8c27d8d3804985e8c3441f56bbb
Sylvain Barthélémy <sylbarth@gmail.com>
2023-11-23 09:44:40 +0100

    azure mode

--- gwenlake/chat_models/openai.py
+++ gwenlake/chat_models/openai.py
@@ -2,7 +2,8 @@ import os
 import logging
 import openai
 from typing import Optional
-from gwenlake.schema import Message
+
+from gwenlake.schema import Message, ChatCompletion
 
 
 logger = logging.getLogger(__name__)
@@ -10,37 +11,48 @@ logger = logging.getLogger(__name__)
 
 class ChatOpenAI():
  
-    def __init__(self, api_base: Optional[str] = None, api_key: Optional[str] = None, model: str = "gpt-3.5-turbo-16k", temperature=0.0):
+    def __init__(self, api_key: Optional[str] = None, api_version: Optional[str] = "2023-05-15", azure_endpoint: Optional[str] = None, model: str = "gpt-3.5-turbo-16k", temperature=0.0):
+
         self.model = model
         self.temperature = temperature
+
+        openai.api_key = api_key or os.environ.get("OPENAI_API_KEY")
+
         if os.environ.get("OPENAI_API_TYPE") == "azure":
             logger.warning("ChatOpenAI: Azure mode.")
-            openai.api_key     = api_key or os.environ.get("OPENAI_API_KEY")
-            openai.api_base    = api_base or os.environ.get("OPENAI_API_BASE")
-            openai.api_type    = "azure"
-            openai.api_version = "2023-05-15" 
+            _version  = api_version or os.environ.get("OPENAI_API_VERSION")
+            _endpoint = azure_endpoint or os.environ.get("OPENAI_API_BASE")
+            self.client = openai.AzureOpenAI(api_version=_version, azure_endpoint=_endpoint)
+
         else:
             logger.warning("ChatOpenAI: OpenAI mode.")
-            openai.api_key = api_key or os.environ.get('OPENAI_API_KEY')
             if os.environ.get('OPENAI_API_ORGANIZATION'):
                 openai.organization = os.environ.get('OPENAI_API_ORGANIZATION')
+            self.client = openai.OpenAI()
 
-    def chat(self, messages: list[Message], stream=False):
+
+    def chat(self, messages: list[Message]):
         try:
-            client = openai.OpenAI()
-            response = client.chat.completions.create(model=self.model, messages=messages, temperature=self.temperature, stream=stream)
+            response = self.client.chat.completions.create(model=self.model, messages=messages, temperature=self.temperature)
         except Exception as e:
             logger.error(e)
             return None
-        return response
+        if not response.choices[0].message.content:
+            return None
+        return ChatCompletion(message=Message(role="assistant", content=response.choices[0].message.content))
+
 
     def stream(self, messages: list[Message]):
         content = ""
-        response = self.chat(messages=messages, stream=True)
+        try:
+            response = self.client.chat.completions.create(model=self.model, messages=messages, temperature=self.temperature, stream=True)
+        except Exception as e:
+            logger.error(e)
+            return None
         for chunk in response:
             if not chunk.choices[0].finish_reason:
                 if chunk.choices[0].delta.content:
                     content += chunk.choices[0].delta.content
                     yield chunk.choices[0].delta.content
             else:
-                yield dict(role="assistant", content=content)
+                yield ChatCompletion(message=Message(role="assistant", content=content))
--- gwenlake/schema.py
+++ gwenlake/schema.py
@@ -1,8 +1,14 @@
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
+from uuid import uuid4
 
 
 class Message(BaseModel):
     role: str
     content: str
+
+class ChatCompletion(BaseModel):
+    id: str = Field(default_factory=lambda: "chatcmpl-" + str(uuid4()))
+    object: str = "chat.completion"
+    message: Message
