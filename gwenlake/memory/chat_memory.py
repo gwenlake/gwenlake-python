49695724de9f4f1e8cd871a1e58823ed3f2da866
Sylvain Barthélémy <sylbarth@gmail.com>
2023-11-24 09:37:21 +0100

    chat

--- gwenlake/memory/chat_memory.py
+++ gwenlake/memory/chat_memory.py
@@ -6,61 +6,64 @@ from gwenlake.utils import num_tokens_from_string
 
 class ChatMemory(ABC):
  
-    def __init__(self, messages: list[Message], max_token_limit=None):
+    def __init__(self, system: str = None, max_token_limit=None):
+        self.system = None
         self.messages = []
-        for message in messages:
-            self.messages.append(Message(**message))
+        if system:
+            self.system = Message(role="system", content=system)
         self.max_token_limit = max_token_limit
-        self.prune()
 
     def __repr__(self):
         text = ""
-        for message in self.messages:
-            text += message.role + ": " + message.content[:50] + "\n"
+        for message in self.get_messages():
+            text += message["role"] + ": " + message["content"][:50] + "\n"
         return text
+
+    def clear(self):
+        self.messages = []
 
     def get_messages(self):
         messages = []
+        if self.system:
+            messages.append(self.system.dict())
         for message in self.messages:
             messages.append(message.dict())
         return messages
 
+    def add_messages(self, messages: list[Message]):
+        for message in messages:
+            self.messages.append(Message(**message))
+        self.prune()
+
     def add_message(self, message: Message):
         self.messages.append(Message(**message))
         self.prune()
-
-    def clear(self):
-        self.messages = []
 
     def prune(self):
 
         if not self.max_token_limit:
             return self.messages
 
+        if not self.messages:
+            return []
+
+        # system
         num_tokens = 0
-        pruned_messages = []
+        if self.system:
+            num_tokens += num_tokens_from_string(self.system.content)
 
-        # add system message
-        for message in self.messages:
-            if message.role == "system":
-                num_tokens += num_tokens_from_string(message.content)
-                pruned_messages.append(message)
-                break
-
-        # filter other messages in reverse order
-        history = []
+        # parse messages in reverse order
+        pruned_history = []
         for message in reversed(self.messages):
-            if message.role != "system":
-                current_tokens = num_tokens+num_tokens_from_string(message.content)
-                if current_tokens > self.max_token_limit:
-                    break
-                history.append(message)
-                num_tokens += num_tokens_from_string(message.content)
+            n = num_tokens + num_tokens_from_string(message.content)
+            if n > self.max_token_limit:
+                break
+            pruned_history.append(message)
+            num_tokens += num_tokens_from_string(message.content)
 
-        if len(history)>0:
-            history.reverse()
-            pruned_messages += history
+        if len(pruned_history)>0:
+            pruned_history.reverse()
 
-        self.messages = pruned_messages
+        self.messages = pruned_history
 
         return self.messages
