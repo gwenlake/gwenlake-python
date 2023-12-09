e80c822065a23946ae341cf119f19f9820f07adb
Sylvain Barthélémy <sylbarth@gmail.com>
2023-12-09 11:14:01 +0100

    embeddings

---
+++ gwenlake/embeddings/__init__.py
@@ -0,0 +1,7 @@
+from gwenlake.embeddings.client import get_embeddings
+from gwenlake.embeddings.langchain import GwenlakeEmbeddings
+
+__all__ = [
+    "get_embeddings",
+    "GwenlakeEmbeddings",
+]
--- gwenlake/embeddings/client.py
+++ gwenlake/embeddings/client.py
@@ -1,10 +1,7 @@
-import logging
-from typing import List
+from typing import Any, Dict, List, Optional
+
 
 from gwenlake.client import Client
-
-logger = logging.getLogger(__name__)
-
 
 def get_embeddings(inputs: List[str], model_id="intfloat/e5-base-v2") -> List[List[float]]:
     return Client().embed(inputs=inputs, model_id=model_id)
