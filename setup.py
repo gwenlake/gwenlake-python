4716c03e7b3832439640b3c9214dfd1a05a72349
Sylvain Barthélémy <sylbarth@gmail.com>
2024-02-21 10:07:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     url="https://github.com/gwenlake/gwenlake-python",
     author="The Gwenlake Team",
     author_email="info@gwenlake.com",
-    install_requires=["httpx", "pydantic", "pyyaml", "numpy", "pandas", "tiktoken"],
-    packages=find_packages(exclude=('tests', "internal-test"))
+    install_requires=["httpx", "pydantic", "pyyaml", "numpy", "pandas", "tiktoken", "liquid"],
+    packages=find_packages(exclude=('tests', "internal-test")),
+    python_requires=">=3.10",  # Specify the minimum Python version required
 )
