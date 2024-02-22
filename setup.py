7b74b6203983b6508602facde7a83714c5fee96e
Sylvain Barthélémy <sylbarth@gmail.com>
2024-02-22 16:31:57 +0100

    liquid

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="gwenlake",
     description = "Python client for Gwenlake API",
-    version='0.1.7',
+    version='0.1.8',
     url="https://github.com/gwenlake/gwenlake-python",
     author="The Gwenlake Team",
     author_email="info@gwenlake.com",
-    install_requires=["httpx", "pydantic", "pyyaml", "numpy", "pandas", "tiktoken", "liquid"],
+    install_requires=["httpx", "pydantic", "pyyaml", "numpy", "pandas", "tiktoken"],
     packages=find_packages(exclude=('tests', "internal-test")),
     python_requires=">=3.10",  # Specify the minimum Python version required
 )
