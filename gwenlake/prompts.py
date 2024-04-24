62dff644dbcb6f422bade4d062812068a504f15e
Sylvain Barthélémy <sylbarth@gmail.com>
2024-04-24 16:27:17 +0200

    format

--- gwenlake/prompts.py
+++ gwenlake/prompts.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-# from langchain.prompts import PromptTemplate
+from langchain.prompts import PromptTemplate
 
 
 from .resource import Resource
@@ -26,9 +26,21 @@ class Prompts(Resource):
         obj = resp.json()
         return obj.get("data")
 
+    def format_prompt(self, messages: list, input: list = []):
+        messages_formated = []
+        for message in messages:
+            t = PromptTemplate.from_template(template=message["content"])
+            if len(t.input_variables)>0:
+                data = { i: input[i] for i in t.input_variables }
+                text = t.format(**data)
+                messages_formated.append({"role": message["role"], "content": text})
+            else:
+                messages_formated.append({"role": message["role"], "content": message["content"]})
+        return messages_formated
 
-    def get(self, ref: str):
+    def get(self, ref: str, input: list = []):
         resp = self._client._request("GET", f"/prompts/{ ref }")
         obj = resp.json()
+        if input:
+            return self.format_prompt(obj["template"], input=input)
         return obj["template"]
-        # return PromptTemplate.from_template(obj["template"])
